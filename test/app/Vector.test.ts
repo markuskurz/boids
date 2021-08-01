d70e0e7effda0a4e5bc50f5c48c8781fc4310d9b
Markus Kurz <markus@activbody.com>
2021-08-01 19:19:28 +0200

    Implement first tests

--- test/app/Vector.test.ts
+++ test/app/Vector.test.ts
@@ -1,7 +1,40 @@
 import { expect } from 'chai';
+import { Vector } from '../../src/app/Vector';
 
 describe('Vector class', () => {
-  it('runs tests', () => {
-    expect(true).to.be.true;
+  describe('length() method', () => {
+    it('calculates the length of vectors', () => {
+      let vector: Vector;
+      vector = new Vector([1, 0]);
+      expect(vector.length()).to.equal(1);
+
+      vector = new Vector([0, 0]);
+      expect(vector.length()).to.equal(0);
+
+      vector = new Vector([0, 1]);
+      expect(vector.length()).to.equal(1);
+
+      vector = new Vector([1, 1]);
+      const length1 = Math.sqrt(2);
+      expect(vector.length()).to.equal(length1);
+
+      vector = new Vector([2, 2]);
+      const length2 = Math.sqrt(8);
+      expect(vector.length()).to.equal(length2);
+    });
+  });
+
+  describe('getDimension() method', () => {
+    it('returns the dimension of vectors', () => {
+      let vector: Vector;
+      vector = new Vector([1, 0]);
+      expect(vector.getDimension()).to.equal(2);
+
+      vector = new Vector([0]);
+      expect(vector.getDimension()).to.equal(1);
+
+      vector = new Vector([1, 2, 3]);
+      expect(vector.getDimension()).to.equal(3);
+    });
   });
 });
